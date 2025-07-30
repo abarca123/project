89cf8b8b447d249f6f3546a47e9fa9a324d5012a
abarca123 <angelineorias1@gmail.com>
2025-07-30 23:15:31 +0800

    Add files via upload

---
+++ ACTIVITY3.java
@@ -0,0 +1,11 @@
+package Grade;
+
+import java.util.Scanner;
+
+public class grade_system {
+    public static void main(String[] args) {
+        
+        int science, history, math, soc, arts;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("+------------------------------------------------------------+");
